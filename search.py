70e5f9f4919afa1e7be7fc16a1c9ecc1af8f4c83
Ariad94 <ariad.94@hotmail.com>
2018-05-05 12:29:47 +0200

    Update search.py

--- search.py
+++ search.py
@@ -1,11 +1,11 @@
-def search(word):
-  file = open(word + ".html", "w")
-  url_beggining = "https://www.google.es/search?q="
-  import requests
-  url = requests.get(url_beggining + word)
-  file.write(url.text)
-  file.close()
+import requests
 
+def search(word):
+  url_beggining = "https://www.google.es/search?q="
+  response = requests.get(url_beggining + word)
+  with open(word + ".html", "w") as file:
+    file.write(response.text)
+    
 if __name__ == "__main__": 
   import sys
   search(sys.argv[1])
