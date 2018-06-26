ffcb24ec974e309110f8d18d176def9e3278cf55
Ariad94 <ariad.94@hotmail.com>
2018-06-27 01:01:05 +0200

    Coherent class

--- names_links3.py
+++ names_links3.py
@@ -1,24 +1,22 @@
-def create_link_table:
-	anchors_names = LinkTable.filtered_names_list("a")
-	cites_names = Link.Table.filtered_names_list("cite")
-	counter = range(len(anchors_names))
-	with open(word + ".txt", "w") as file: 
-		for i in counter:
-			file.write(i+1,"\t",anchors_names[i],"\t",cite_names[i]) 
+def __main__(word):
+	return LinkTable(word)
 
 
-class LinkTable(word):
-
+class LinkTable: 
 	import requests 
-	from bs4 import BeautifulSoup 
-
-	url_beggining = "https://www.google.es/search?q=" 
-	response = requests.get(url_beggining + word) 
-	soup = BeautifulSoup(response.text, "html.parser")
-	
-	div_g = soup.find_all("div", {"class":"g"})
-	div_g = [d for d in div_g if (d.find("a") and d.find("cite"))  is not  None]
-	
+	from bs4 import BeautifulSoup
+	def __init__(self, word):
+		url_beggining = "https://www.google.es/search?q=" 
+		response = requests.get(url_beggining + word) 
+		soup = BeautifulSoup(response.text, "html.parser")
+		div_g = soup.find_all("div", {"class":"g"})
+		self.div_g = [d for d in div_g if (d.find("a") and d.find("cite"))  is not  None]
+		anchors_names = LinkTable.filtered_names_list(self,"a")
+		cites_names = LinkTable.filtered_names_list(self,"cite")
+		counter = range(len(anchors_names))
+		with open(word + ".txt", "w") as file: 
+			for i in counter:
+				file.write(i+1,"\t",anchors_names[i],"\t",cite_names[i])
 	def names_list(self, tag):
 		list = []
 		#list.append(d.find(tag) for d in LinkTable.div_g)
@@ -29,18 +27,14 @@ class LinkTable(word):
 		for l in list:
 			list_names.append(l.text)
 		return list_names
-		
-	cites_names = Link.Table.names_list("cite")
-	
-	filter_index = []
-	for c in cites_names:
-		if c.text[0:3] == "htt" or c.text[0:3] == "www": index.append(cites_names.index(c))
-	
-			
-		
+	def filter_index(self):
+		filter_index = []
+		for c in LinkTable.names_list(self,"cite"):
+			if c.text[0:3] == "htt" or c.text[0:3] == "www": index.append(cites_names.index(c))
+		return filter_index
 	def filtered_names_list(self,tag):
-		return [LinkTable.names_list(tag)[i] for i in filter_index] 
-		
+		return [LinkTable.names_list(tag)[i] for i in LinkTable.filter_index(self)]
+			
 if __name__ == "__main__":  
 	import sys 
 	search(sys.argv[1]) 
